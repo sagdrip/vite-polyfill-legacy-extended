2e80f2802a5a6855723609896e89f65fef4f3c43
sag drip <gulg.2@yandex.com>
2026-06-21 21:10:44 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+import path from "path";
+
+export function legacyPolyfill() {
+  return {
+    name: 'vite-polyfill-legacy-extended',
+    transform(code, id) {
+      const mirror = path.join(import.meta.dirname, path.relative(process.cwd(), id));
+      if (!fs.existsSync(mirror)) return "";
+      return fs.readFileSync(mirror, "utf-8");
+    }
+  };
+}
