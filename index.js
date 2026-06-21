13424aa331ae370d0928cdd0cb7a1257136f84a4
sag drip <gulg.2@yandex.com>
2026-06-21 21:26:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ export function legacyPolyfill() {
     transform(code, id) {
       if (id.startsWith("\0") || id.includes("node_modules")) return null;
 
+      if (!(id.endsWith(".svelte") || id.endsWith(".html") || id.endsWith(".js") || id.endsWith(".ts"))) return null;
+
       const mirror = path.join(import.meta.dirname, path.relative(process.cwd(), id));
       if (!fs.existsSync(mirror)) return " ";
       return fs.readFileSync(mirror, "utf-8");
