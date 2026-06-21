b1897ee7c5529b16060e2e4a9312e6aa4c9096a8
sag drip <gulg.2@yandex.com>
2026-06-21 21:12:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ export function legacyPolyfill() {
     name: 'vite-polyfill-legacy-extended',
     transform(code, id) {
       const mirror = path.join(import.meta.dirname, path.relative(process.cwd(), id));
+      console.log(id, mirror);
       if (!fs.existsSync(mirror)) return "";
       return fs.readFileSync(mirror, "utf-8");
     }
